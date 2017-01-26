42fa9ada47e18edf863db07c712b8356821a5a9f
Anant Shrivastava <anant@anantshri.info>
2017-01-26 20:28:22 +0530

    tweaked for less pipes

--- packtpub_free.sh
+++ packtpub_free.sh
@@ -3,6 +3,6 @@
 # Create a notification icon to show what's available for free at 
 # https://www.packtpub.com/packt/offers/free-learning
 # TODO: FIX : clicking on notification opens apple script editor, should open browser with url.
-title=`curl https://www.packtpub.com/packt/offers/free-learning/ 2>/dev/null | grep -A2 "dotd-title" | tr "\n" " " | tr -s " " | tr -s "\t" " " | cut -f3  -d">" | cut -f1 -d"<"`
+title=`curl https://www.packtpub.com/packt/offers/free-learning/ 2>/dev/null | grep -A2 "dotd-title" | cut -f1 -d"<" | tr -d "\t\n"`
 caption="Free Packtpub Book"
 /usr/bin/osascript -e "display notification \"$caption\" with title \"$title\""
